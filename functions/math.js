8b670a1565f657f81afbbebab22f6e7daaa991fb
Rana Salem <ranatasalem@gmail.com>
2024-09-11 12:18:45 +0100

    convert commonjs to esm

--- functions/math.js
+++ functions/math.js
@@ -1,12 +1,12 @@
 function multiply(a, b) {
-  return a * b;
+  return a * b
 }
 
 function divide(a, b) {
   if (b === 0) {
-    throw new Error("Division by zero is not allowed");
+    throw new Error("Division by zero is not allowed")
   }
-  return a / b;
+  return a / b
 }
 
-module.exports = { multiply, divide };
+export { multiply, divide }
